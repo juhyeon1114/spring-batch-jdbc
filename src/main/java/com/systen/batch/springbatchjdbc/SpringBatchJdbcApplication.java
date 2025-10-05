2bbce657a337922292b354da9d4645f615988fde
juhyeon1114 <juhyeon1114@gmail.com>
2025-10-05 23:47:44 +0900

    init

---
+++ src/main/java/com/systen/batch/springbatchjdbc/SpringBatchJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.systen.batch.springbatchjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systen/batch/springbatchjdbc/SpringBatchJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systen.batch.springbatchjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
