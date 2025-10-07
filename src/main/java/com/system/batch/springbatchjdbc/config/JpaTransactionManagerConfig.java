67b01d63e2865d8beefbd5000399a97c86c3044e
juhyeon1114 <juhyeon1114@gmail.com>
2025-10-07 22:30:56 +0900

    feat: Spring Batch 작업(PostPageJob) 추가 및 PostCursorJob 비활성화

---
+++ src/main/java/com/system/batch/springbatchjdbc/config/JpaTransactionManagerConfig.java
@@ -0,0 +1,17 @@
+package com.system.batch.springbatchjdbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+public class JpaTransactionManagerConfig {
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+
+}
--- src/main/java/com/system/batch/springbatchjdbc/config/PostJpaPageConfig.java
+++ src/main/java/com/system/batch/springbatchjdbc/config/PostJpaPageConfig.java
@@ -4,58 +4,54 @@ import java.util.Map;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaCursorItemReader;
-import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.system.batch.springbatchjdbc.entity.Post;
 
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// @Configuration
+@Configuration
 @RequiredArgsConstructor
-public class PostJpaCursorConfig {
+public class PostJpaPageConfig {
 	private final JobRepository jobRepository;
 	private final PlatformTransactionManager transactionManager;
 	private final EntityManagerFactory entityManagerFactory;
-	@PersistenceContext
-	private EntityManager entityManager;
 
 	@Bean
-	public Job postCursorJob() {
-		return new JobBuilder("postCursorJob", jobRepository)
-			.start(postCursorStep())
+	public Job postJob() {
+		return new JobBuilder("postJob", jobRepository)
+			.start(postStep())
 			.build();
 	}
 
 	@Bean
-	public Step postCursorStep() {
-		return new StepBuilder("postCursorStep", jobRepository)
+	public Step postStep() {
+		return new StepBuilder("postStep", jobRepository)
 			.<Post, Post>chunk(3, transactionManager)
-			.reader(postCursorReader())
-			.processor(postCursorProcessor())
+			.reader(postPageReader())
+			.processor(postPageProcessor())
 			.writer(postWriter())
 			.allowStartIfComplete(true)
 			.build();
 	}
 
 	@Bean
-	public JpaCursorItemReader<Post> postCursorReader() {
-		return new JpaCursorItemReaderBuilder<Post>()
-			.name("postCursorReader")
+	public JpaPagingItemReader<Post> postPageReader() {
+		return new JpaPagingItemReaderBuilder<Post>()
+			.name("postPageReader")
 			.entityManagerFactory(entityManagerFactory)
 			.queryString("""
 				SELECT p FROM Post p
@@ -65,24 +61,30 @@ public class PostJpaCursorConfig {
 			.parameterValues(Map.of(
 				"id", 9
 			))
+			.pageSize(3)
+			// transacted == true: 데이터 조회 전, entityManager.flush()가 호출됨
+			// transacted == false: 데이터 조회 후, entityManager.detach()를 실행하여, 영속성 컨텍스트에서 관리되지 않도록 한다.
+			.transacted(false)
 			.build();
 	}
 
 	@Bean
-	public ItemProcessor<Post, Post> postCursorProcessor() {
+	public ItemProcessor<Post, Post> postPageProcessor() {
 		return item -> {
 			boolean isEven = item.getId() % 2 == 0;
-			return isEven ? item : null;
+			if (!isEven) {
+				return null;
+			}
+			item.addViews();
+			return item;
 		};
 	}
 
 	@Bean
-	public ItemWriter<Post> postWriter() {
-		return items -> items.forEach(item -> {
-			log.info("postWriter: {}", item);
-			Post post = entityManager.merge(item);
-			post.addViews();
-		});
+	public JpaItemWriter<Post> postWriter() {
+		return new JpaItemWriterBuilder<Post>()
+			.entityManagerFactory(entityManagerFactory)
+			.build();
 	}
 
 }
